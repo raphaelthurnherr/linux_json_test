f905e254181fb2e16f3f90f845d7e42fca7b5ff5
raph <raphael.thurnherr@gmail.com>
2016-03-03 16:20:12 +0100

    beta test

---
+++ linux_JSON/src/linux_json.h
@@ -0,0 +1,24 @@
+
+// DEFINITION DES TYPES DE MESSAGE
+
+#define COMMAND 0
+#define REQUEST 1
+#define ACK     2
+#define RESPONSE 3
+#define EVENT    4
+#define NEGOC    5
+#define ERROR    6
+
+// DEFINITION DES PARAMETRES COMMANDE
+
+#define STOP     0
+#define FORWARD  1
+#define BACK     2
+#define LEFT     3
+#define RIGHT    4
+#define ROTATE_LEFT 5
+#define ROTATE_RIGHT 6
+#define FORWARD_SPIN_LEFT 7
+#define FORWARD_SPIN_RIGHT 8
+#define BACK_SPIN_LEFT 9
+#define BACK_SPIN_RIGHT 10
